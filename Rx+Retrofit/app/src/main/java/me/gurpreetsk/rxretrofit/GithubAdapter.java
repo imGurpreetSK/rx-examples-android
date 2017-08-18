8f780497d8dd7f0573b40691f01821a890226397
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-08-18 17:08:21 +0530

    feat: fetching data using retrofit nd RxJava

--- Rx+Retrofit/app/src/main/java/me/gurpreetsk/rxretrofit/GithubAdapter.java
+++ Rx+Retrofit/app/src/main/java/me/gurpreetsk/rxretrofit/GithubAdapter.java
@@ -5,8 +5,10 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import me.gurpreetsk.rxretrofit.model.Project;
 
@@ -14,15 +16,15 @@ import me.gurpreetsk.rxretrofit.model.Project;
  * Created by Gurpreet on 18/08/17.
  */
 
-public class GithubAdapter extends RecyclerView.Adapter<GithubAdapter.MyViewHolder> {
+public class GithubAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
     private Context context;
-    private ArrayList<Project> projects;
+    private List<Project> projects;
 
     private static final String TAG = GithubAdapter.class.getSimpleName();
 
 
-    public GithubAdapter(Context context, ArrayList<Project> projects) {
+    public GithubAdapter(Context context, List<Project> projects) {
         this.context = context;
         this.projects = projects;
     }
@@ -35,7 +37,9 @@ public class GithubAdapter extends RecyclerView.Adapter<GithubAdapter.MyViewHold
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-
+        holder.textViewName.setText(projects.get(holder.getAdapterPosition()).getName());
+        holder.textViewUrl.setText(projects.get(holder.getAdapterPosition()).getUrl());
+        holder.textViewDesc.setText(projects.get(holder.getAdapterPosition()).getDescription());
     }
 
     @Override
@@ -43,11 +47,18 @@ public class GithubAdapter extends RecyclerView.Adapter<GithubAdapter.MyViewHold
         return projects.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
+}
 
-        public MyViewHolder(View itemView) {
-            super(itemView);
-        }
+class MyViewHolder extends RecyclerView.ViewHolder {
+
+    TextView textViewName;
+    TextView textViewUrl;
+    TextView textViewDesc;
+
+    MyViewHolder(View view) {
+        super(view);
+        textViewName = (TextView) view.findViewById(R.id.textview_name);
+        textViewUrl = (TextView) view.findViewById(R.id.textview_url);
+        textViewDesc = (TextView) view.findViewById(R.id.textview_description);
     }
-
 }
--- Rx+Retrofit/app/src/main/java/me/gurpreetsk/rxretrofit/MainActivity.java
+++ Rx+Retrofit/app/src/main/java/me/gurpreetsk/rxretrofit/MainActivity.java
@@ -2,8 +2,12 @@ package me.gurpreetsk.rxretrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import io.reactivex.Observer;
@@ -20,6 +24,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    ProgressBar progressBar;
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
@@ -28,13 +33,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        progressBar.setIndeterminate(true);
+        progressBar.setVisibility(View.VISIBLE);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this,
+                DividerItemDecoration.VERTICAL));
 
         ApiInterface apiService =
                 ApiClient.getClient().create(ApiInterface.class);
 
-        apiService.getGithubRepos("rx")
+        apiService.getGithubRepos("rxjava")
                 .subscribeOn(Schedulers.io())   //create on io thread
                 .observeOn(AndroidSchedulers.mainThread())  //observe the response on Main Thread
+                .retry(5)
                 .subscribe(new Observer<Response<GithubResults>>() {
                     Disposable d;
 
@@ -45,9 +58,14 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onNext(@NonNull Response<GithubResults> githubResultsResponse) {
+                        //The results are presented here
+                        progressBar.setVisibility(View.GONE);
                         Log.d(TAG, "onNext: code: " + githubResultsResponse.code());
+
                         if (githubResultsResponse.code() == 200) {
                             Log.i(TAG, "onNext: " + githubResultsResponse.body().getItems().size());
+                            recyclerView.setAdapter(
+                                    new GithubAdapter(MainActivity.this, githubResultsResponse.body().getItems()));
                         } else {
                             Log.e(TAG, "onNext: error: " + githubResultsResponse.errorBody().toString());
                         }
@@ -55,16 +73,18 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void onError(@NonNull Throwable e) {
+                        progressBar.setVisibility(View.GONE);
                         Toast.makeText(MainActivity.this,
                                 e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-                        Log.e(TAG, "onError: ", e);
+                        e.printStackTrace();
                         if (!d.isDisposed())    //dispose the resource if not done already
                             d.dispose();
                     }
 
                     @Override
                     public void onComplete() {
-                        if (!d.isDisposed())
+                        progressBar.setVisibility(View.GONE);
+                        if (!d.isDisposed())    //dispose the resource if not done already
                             d.dispose();
                     }
                 });
