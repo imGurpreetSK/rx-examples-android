3ce45f1b94464e5e80331a8a6dec3695f1341b0f
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-08-25 23:07:32 +0530

    docs: changes in SubstituteAsynctask

--- SubstituteAsynctask/app/src/main/java/me/gurpreetsk/asynctasksubstitute/MainActivity.java
+++ SubstituteAsynctask/app/src/main/java/me/gurpreetsk/asynctasksubstitute/MainActivity.java
@@ -25,69 +25,69 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+  private static final String TAG = MainActivity.class.getSimpleName();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        final TextView textview = (TextView) findViewById(R.id.textview);
+    final TextView textview = (TextView) findViewById(R.id.textview);
 
-        getObservable()
-                // Do all the work on the computation thread.
-                // Other options are io, newThread
-                .observeOn(Schedulers.computation())
-                // Map function does some operation on each element emitted and returns an observable
-                .map(new Function<List<Integer>, Integer>() {
-                    // Gets data from 'e.onNext(Arrays.asList(1, 2, 3, 4, 5))'
-                    @Override
-                    public Integer apply(@NonNull List<Integer> integers) throws Exception {
-                        int prod = 1;
-                        for (int i : integers) {
-                            prod *= i;
-                            SystemClock.sleep(500);
-                        }
-                        return prod;
-                    }
-                })
-                // All operations below this will happen on Android's MainThread
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Integer>() {
-                    Disposable d;
-
-                    @Override
-                    public void onSubscribe(@NonNull Disposable d) {
-                        this.d = d;
-                    }
-
-                    @Override
-                    public void onNext(@NonNull Integer prod) {
-                        //Update UI
-                        textview.setText("The product is " + prod);
-                    }
-
-                    @Override
-                    public void onError(@NonNull Throwable e) {
-                        e.printStackTrace();
-                    }
-
-                    @Override
-                    public void onComplete() {
-                        if (!d.isDisposed())
-                            d.dispose();
-                    }
-                });
-
-    }
-
-    private Observable<List<Integer>> getObservable() {
-        return Observable.create(new ObservableOnSubscribe<List<Integer>>() {
-            @Override
-            public void subscribe(@NonNull ObservableEmitter<List<Integer>> e) throws Exception {
-                e.onNext(Arrays.asList(1, 2, 3, 4, 5));
+    getObservable()
+        // Do all the work on the computation thread.
+        // Other options are io, newThread
+        .observeOn(Schedulers.computation())
+        // Map function does some operation on each element emitted and returns an observable
+        .map(new Function<List<Integer>, Integer>() {
+          // Gets data from 'e.onNext(Arrays.asList(1, 2, 3, 4, 5))'
+          @Override
+          public Integer apply(@NonNull List<Integer> integers) throws Exception {
+            int prod = 1;
+            for (int i : integers) {
+              prod *= i;
+              SystemClock.sleep(700);
             }
+            return prod;
+          }
+        })
+        // All operations below this will happen on Android's MainThread
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Observer<Integer>() {
+          Disposable d;
+
+          @Override
+          public void onSubscribe(@NonNull Disposable d) {
+            this.d = d;
+          }
+
+          @Override
+          public void onNext(@NonNull Integer prod) {
+            //Update UI
+            textview.setText("The product is " + prod);
+          }
+
+          @Override
+          public void onError(@NonNull Throwable e) {
+            e.printStackTrace();
+          }
+
+          @Override
+          public void onComplete() {
+            if (!d.isDisposed())
+              d.dispose();
+          }
         });
-    }
+
+  }
+
+  private Observable<List<Integer>> getObservable() {
+    return Observable.create(new ObservableOnSubscribe<List<Integer>>() {
+      @Override
+      public void subscribe(@NonNull ObservableEmitter<List<Integer>> e) throws Exception {
+        e.onNext(Arrays.asList(1, 2, 3, 4, 5));
+      }
+    });
+  }
 
 }
