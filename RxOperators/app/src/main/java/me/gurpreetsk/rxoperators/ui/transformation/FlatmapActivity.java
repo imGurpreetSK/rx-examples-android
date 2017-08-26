4410f3a3ead640e6d4455d9107429d0c045374ad
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-08-26 22:31:54 +0530

    feat: add FlatMap example

--- RxOperators/app/src/main/java/me/gurpreetsk/rxoperators/ui/transformation/FlatmapActivity.java
+++ RxOperators/app/src/main/java/me/gurpreetsk/rxoperators/ui/transformation/FlatmapActivity.java
@@ -36,7 +36,26 @@ public class FlatmapActivity extends AppCompatActivity {
   @Override
   protected void onStart() {
     super.onStart();
-
+    disposable = Observable.just(2, 4, 6, 8, 10)
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.io())
+        // the flatmap operator does some processing on each element passed by the observable to it
+        // and returns an *observable* for each element
+        //(see https://stackoverflow.com/questions/22847105/when-do-you-use-map-vs-flatmap-in-rxjava)
+        .flatMap(new Function<Integer, ObservableSource<Integer>>() {
+          @Override
+          public ObservableSource<Integer> apply(@NonNull Integer integer) throws Exception {
+            return Observable.just(1, 2, 3);
+          }
+        })
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(new Consumer<Integer>() {
+          @Override
+          public void accept(Integer integer) throws Exception {
+            Log.i(TAG, "accept: " + integer);
+            textviewFlatmap.setText(textviewFlatmap.getText() + " " + integer);
+          }
+        });
   }
 
   @Override
