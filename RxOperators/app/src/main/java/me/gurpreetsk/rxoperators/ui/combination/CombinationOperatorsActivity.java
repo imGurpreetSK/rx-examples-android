e0fd9691f1d958247fa43167e6ccf8f866a0c287
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-09-03 13:54:32 +0530

    feat: setup CombinationOperatorActivity

--- RxOperators/app/src/main/java/me/gurpreetsk/rxoperators/ui/combination/CombinationOperatorsActivity.java
+++ RxOperators/app/src/main/java/me/gurpreetsk/rxoperators/ui/combination/CombinationOperatorsActivity.java
@@ -1,8 +1,10 @@
 package me.gurpreetsk.rxoperators.ui.combination;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import me.gurpreetsk.rxoperators.R;
 
 public class CombinationOperatorsActivity extends AppCompatActivity {
@@ -13,6 +15,33 @@ public class CombinationOperatorsActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_combinational_operators);
+    ButterKnife.bind(this);
     setTitle(TAG);
   }
+
+  @OnClick(R.id.button_merge)
+  public void openMergeActivity() {
+
+  }
+
+  @OnClick(R.id.button_concat)
+  public void openConcatActivity() {
+
+  }
+
+  @OnClick(R.id.button_zip)
+  public void openZipActivity() {
+
+  }
+
+  @OnClick(R.id.button_combine_latest)
+  public void openCombineLatestActivity() {
+
+  }
+
+  @OnClick(R.id.button_race)
+  public void openRaceActivity() {
+
+  }
+
 }
