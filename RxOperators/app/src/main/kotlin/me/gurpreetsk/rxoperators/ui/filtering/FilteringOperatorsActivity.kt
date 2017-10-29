f6e7cf79ee3886e576e56a7e7f2d06796a372b45
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-10-29 17:18:02 +0530

    Add layout for FilteringActivity

--- RxOperators/app/src/main/kotlin/me/gurpreetsk/rxoperators/ui/filtering/FilteringOperatorsActivity.kt
+++ RxOperators/app/src/main/kotlin/me/gurpreetsk/rxoperators/ui/filtering/FilteringOperatorsActivity.kt
@@ -5,9 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import me.gurpreetsk.rxoperators.R
 
 class FilteringOperatorsActivity : AppCompatActivity() {
-    companion object {
-        private val TAG = FilteringOperatorsActivity::class.java.simpleName
-    }
+    private val TAG = FilteringOperatorsActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
