33b5881b348d7a08c50b1bf4a0dd81f74e21f547
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-10-29 15:04:17 +0530

    Setup different souceSets, move classes

---
+++ RxOperators/app/src/main/kotlin/me/gurpreetsk/rxoperators/ui/filtering/FilteringOperatorsActivity.kt
@@ -0,0 +1,17 @@
+package me.gurpreetsk.rxoperators.ui.filtering
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import me.gurpreetsk.rxoperators.R
+
+class FilteringOperatorsActivity : AppCompatActivity() {
+    companion object {
+        private val TAG = FilteringOperatorsActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filtering_operators)
+        title = TAG
+    }
+}
