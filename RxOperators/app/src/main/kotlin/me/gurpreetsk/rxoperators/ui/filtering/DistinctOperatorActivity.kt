466a4e877af6a2256196eea54e31432ad7cb021e
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-10-31 16:44:33 +0530

    DistinctOperatorActivity

--- RxOperators/app/src/main/kotlin/me/gurpreetsk/rxoperators/ui/filtering/DistinctOperatorActivity.kt
+++ RxOperators/app/src/main/kotlin/me/gurpreetsk/rxoperators/ui/filtering/DistinctOperatorActivity.kt
@@ -2,14 +2,27 @@ package me.gurpreetsk.rxoperators.ui.filtering
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import me.gurpreetsk.rxoperators.R
 
 class DistinctOperatorActivity : AppCompatActivity() {
-    private val TAG = DistinctOperatorActivity::class.simpleName
+  private val TAG = DistinctOperatorActivity::class.simpleName
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_distinct_operator)
-        title = TAG
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_distinct_operator)
+    title = TAG
+  }
+
+  override fun onStart() {
+    super.onStart()
+    Observable.just("gurpreet", "jatin", "ashish", "gurpreet", "afnan")
+        .subscribeOn(Schedulers.io())
+        .distinct()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe({ Log.i(TAG, ": $it"); })
+  }
 }
